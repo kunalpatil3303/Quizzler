78c1cfcede47a664363fd502da8b0afd52213834
KUNAL <kunalpatil3303@gmail.com>
2025-08-27 18:25:15 -0400

    Lesson 90

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,14 @@ class Quizzler extends StatelessWidget {
 
 class QuizPage extends StatefulWidget {
   List<Widget> scoreKeeper = [];
+  List<String> question = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
+
   @override
   _QuizPageState createState() => _QuizPageState();
 }
@@ -38,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                widget.question[widget.questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -59,9 +67,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  widget.scoreKeeper.add(
-                    Icon(Icons.check, color: Colors.green),
-                  );
+                  widget.questionNumber++;
                 });
               },
             ),
@@ -81,7 +87,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  widget.scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  widget.questionNumber++;
                 });
               },
             ),
